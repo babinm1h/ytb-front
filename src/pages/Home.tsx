e8d2393cfc22e33b939c86d8a377e981ff5f3ea8
babinm1h <babinmih@mail.ru>
2022-06-23 18:20:57 +0300

    fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,56 +1,45 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import Slider from '../components/Slider';
 import MainLayout from '../components/layouts/MainLayout';
 import VideosList from '../components/VideosList';
-import { fetchAllVideos, fetchPopularUsers, fetchPopularVideos } from '../redux/thunks/videos.thunks';
+import { fetchPopularUsers, fetchPopularVideos } from '../redux/thunks/videos.thunks';
 import { useAppSelector } from '../hooks/useAppSelector';
 import { useAppDispatch } from '../hooks/useAppDispatch';
 import ChannelsList from '../components/ChannelsList';
 import { useScrollPagination } from '../hooks/useScrollPagination';
 import VideoSkeletonList from '../components/Loaders/VideoSkeletonList';
-import useScroll from '../hooks/useScroll';
-import { incrCurrentPage } from '../redux/slices/videos.slice';
 
 const Home = () => {
-    const parentRef = useRef(null)
-    const childRef = useRef(null)
-
     const dispatch = useAppDispatch()
     const { isLoading, videos, popularVideos, popularUsers, isUsersLoading, currentPage, totalCount } = useAppSelector(state => state.videos)
-
 
     useEffect(() => {
         dispatch(fetchPopularVideos())
         dispatch(fetchPopularUsers())
     }, [dispatch])
 
-    const cb = useCallback(() => dispatch(fetchAllVideos(currentPage)).then(() => dispatch(incrCurrentPage())), [])
+    
 
-    // const scrollPagination = useScrollPagination(videos.length, currentPage, totalCount)
-
-    const scr = useScroll(parentRef, childRef, cb)
+    const scrollPagination = useScrollPagination(videos.length, currentPage, totalCount)
 
     return (
         <>
             <MainLayout>
-                <div className="w-full h-full overflow-auto" ref={parentRef}>
-                    <section className="p-5 border-b border-gray-300">
-                        <h2 className="sectionTitle">Popular Channels</h2>
-                        {popularUsers.length > 0 && <ChannelsList users={popularUsers} isLoading={isUsersLoading} />}
-                    </section>
+                <section className="p-5 border-b border-gray-300">
+                    <h2 className="sectionTitle">Popular Channels</h2>
+                    {popularUsers.length > 0 && <ChannelsList users={popularUsers} isLoading={isUsersLoading} />}
+                </section>
 
-                    <section className="hidden sm:block p-5 border-b border-gray-300">
-                        <h2 className="sectionTitle">Popular Videos</h2>
-                        <Slider videos={popularVideos} />
-                    </section>
+                <section className="hidden sm:block p-5 border-b border-gray-300">
+                    <h2 className="sectionTitle">Popular Videos</h2>
+                    <Slider videos={popularVideos} />
+                </section>
 
-                    <section className="p-5">
-                        <h2 className="sectionTitle">Recommended</h2>
-                        <VideosList videos={videos} />
-                        {isLoading && <VideoSkeletonList />}
-                    </section>
-                    <div ref={childRef}></div>
-                </div>
+                <section className="p-5" >
+                    <h2 className="sectionTitle">Recommended</h2>
+                    <VideosList videos={videos} />
+                    {isLoading && <VideoSkeletonList />}
+                </section>
 
             </MainLayout>
         </>
