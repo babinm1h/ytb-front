cdd052627a1c0a017f0968d346132c3b57488a8f
@babinm1h <babinmih@mail.ru>
2022-12-17 20:03:06 +0300

    redeploy

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -1,29 +1,29 @@
-import React, { useEffect } from "react"
-import { useParams } from "react-router-dom"
-import MainLayout from "../components/layouts/MainLayout"
-import CommentsForm from "../components/UI/forms/CommentsForm"
-import VideoInfo from "../components/VideoPage/VideoInfo"
-import { useAppDispatch } from "../hooks/useAppDispatch"
-import { useAppSelector } from "../hooks/useAppSelector"
-import { resetVideo } from "../redux/slices/videoPage.slice"
-import { fetchSingleVideo, fetchVideoComments } from "../redux/thunks/videoPage.thunks"
-import ReactPlayer from "react-player"
-import CommentsList from "../components/VideoPage/CommentsList"
-import Spinner from "../components/Loaders/Spinner"
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import MainLayout from "../components/layouts/MainLayout";
+import CommentsForm from "../components/UI/forms/CommentsForm";
+import VideoInfo from "../components/VideoPage/VideoInfo";
+import { useAppDispatch } from "../hooks/useAppDispatch";
+import { useAppSelector } from "../hooks/useAppSelector";
+import { resetVideo } from "../redux/slices/videoPage.slice";
+import { fetchSingleVideo, fetchVideoComments } from "../redux/thunks/videoPage.thunks";
+import ReactPlayer from "react-player";
+import CommentsList from "../components/VideoPage/CommentsList";
+import Spinner from "../components/Loaders/Spinner";
 
 const VideoPage = () => {
-  const { id } = useParams() as { id: string }
-  const dispatch = useAppDispatch()
-  const { video, isVideoLoading, comments } = useAppSelector((state) => state.videoPage)
-  const { user } = useAppSelector((state) => state.auth)
+  const { id } = useParams() as { id: string };
+  const dispatch = useAppDispatch();
+  const { video, isVideoLoading, comments } = useAppSelector((state) => state.videoPage);
+  const { user } = useAppSelector((state) => state.auth);
 
   useEffect(() => {
-    dispatch(fetchSingleVideo(id))
-    dispatch(fetchVideoComments(id))
+    dispatch(fetchSingleVideo(id));
+    dispatch(fetchVideoComments(id));
     return () => {
-      dispatch(resetVideo())
-    }
-  }, [id])
+      dispatch(resetVideo());
+    };
+  }, [id]);
 
   return (
     <MainLayout>
@@ -34,8 +34,8 @@ const VideoPage = () => {
           </div>
         ) : (
           <>
-            <div className="w-full h-auto border border-gray-400 bg-black">
-              <ReactPlayer url={video.video} controls={true} width="100%" height="100%" volume={0.1} />
+            <div className="w-full h-auto border border-gray-400 bg-black max-h-[600px]">
+              <ReactPlayer url={video.video} controls={true} width="100%" height="600px" volume={0.1} cla />
             </div>
             <VideoInfo video={video} />
             <div className="py-5">
@@ -47,7 +47,7 @@ const VideoPage = () => {
         )}
       </div>
     </MainLayout>
-  )
-}
+  );
+};
 
-export default VideoPage
+export default VideoPage;
